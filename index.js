c4e8ebaf310ccc6de0b2e4b85a2f699516df0044
Kunju Perath <kperath@gmail.com>
2020-07-19 18:13:58 -0400

    can get user id and delete user with id from database

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ app.get("/", (req, res) => {
     res.send("<h1>Heroes User API</h1>");
 });
 
+// gets id of user
+app.get("/api/user/id", (req, res) => {
+    const data = [req.body.username, req.body.password];
+    const sql = "SELECT id FROM user WHERE username = ? AND password = ?";
+    db.get(sql, data, (err, row) => {
+        if (err) console.log(err.message);
+        else {
+            if (!row) res.status(404).sendStatus("User Id does not exist");
+            else res.send(row);
+        }
+    });
+})
+
+// register a user
 app.post("/api/user/add", (req, res) => {
     const data = [req.body.username, req.body.password]
     const sql = "INSERT INTO user (username, password) VALUES (?, ?)";
@@ -25,8 +39,15 @@ app.post("/api/user/add", (req, res) => {
 
 // inside heroes app or user account app (where user can change password) or delete account
 // client side should make user enter password to confirm deletion
+// remove based on id
 app.delete("/api/user/remove", (req, res) => {
-
+    const sql = `DELETE FROM user where id = ${req.body.id}`;
+    db.run(sql, function (err) {
+        if (err) console.log(err.message);
+        else {
+            res.send("User deleted.");
+        }
+    })
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
